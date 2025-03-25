3c8102178aa1a0be9eddf390fb206269a23c1f7a
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-03-25 12:26:17 +0700

    Perulangan

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -15,5 +15,19 @@ void input()
         {
             break;
         }
+        else
+        {
+cout << "\nArray yang anda masukan maksimal 20 Elemen.\n";
+        }
+    }
+    cout << endl;
+    cout << "=================" << endl;
+    cout << "Masukan Element Array" << endl;
+    cout << "=================" << endl;
+
+    for (int i =0; i < n; i++)
+    {
+cout << " Data ke-" << (i + 1) << ":" ;
+cin >> arr[1];
     }
 }
