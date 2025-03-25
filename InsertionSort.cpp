bb552af99d86a897271521d241909a8819c364f5
rizkizulfauzi <rizkizulfauzi18@gmail.com>
2025-03-25 12:31:12 +0700

    Prosedur InsertionSort

--- InsertionSort.cpp
+++ InsertionSort.cpp
@@ -31,3 +31,22 @@ cout << " Data ke-" << (i + 1) << ":" ;
 cin >> arr[1];
     }
 }
+
+void insertionSort()
+{
+    int  temp;
+    int j,i;
+    for (i = 1; i <= n -1; i++)
+    {
+        temp = arr[i];
+         j = i -1;
+         while (j >= 0 && arr[j] > temp)
+         {
+            arr[j + 1] = arr [j];
+            j--;
+            
+            
+         }
+         arr[j +1] = temp;
+    }
+}
